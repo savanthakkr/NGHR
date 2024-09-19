7851a9e28efe12453d7eae6f43a2773cc0fb15cf
Jigisha <jigishaadatiya21@gmail.com>
2024-09-19 17:32:36 +0530

    minor css changes for responsive view under the client tab

--- src/pages/consultant/clients.js
+++ src/pages/consultant/clients.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ConsultantLayout from "../../layout/ConsultantLayout";
 import ConsultantClient from "../../components/consultant/ConsultantClient";
-import ConsultantChat from "../../components/consultant/ConsultantChat";
 
 function Clients() {
     return (
